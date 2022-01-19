c2e1467bc09da9972d3681749560593b49832199
bsko <bsko@tracom.kr>
2022-01-19 13:09:13 +0900

    [고병수] BIMS 경로 수정, 차내장치 종류별 파라미터 프로그램 생성

---
+++ BMS-Web/src/main/java/kr/tracom/bms/domain/VD0101/VD0101Mapper.java
@@ -0,0 +1,16 @@
+package kr.tracom.bms.domain.VD0101;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface VD0101Mapper {
+	/*public List<Map> PI0100G0R0(Map param);
+	public Map PI0100G0K0();
+	public List PI0100SHI0();
+	public int PI0100G0I0(Map param);
+	public int PI0100G0D0(Map param);
+	public int PI0100G0U0(Map param);*/
+}
