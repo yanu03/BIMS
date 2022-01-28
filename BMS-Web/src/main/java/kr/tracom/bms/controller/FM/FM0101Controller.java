b9b2935978af25d8a62ca0ddca3f4b7e75c7a190
bsko <bsko@tracom.kr>
2022-01-28 11:30:27 +0900

    [고병수] 차내장치 파라미터, 시설물 파라미터 임시 커밋.

---
+++ BMS-Web/src/main/java/kr/tracom/bms/controller/FM/FM0101Controller.java
@@ -0,0 +1,70 @@
+package kr.tracom.bms.controller.FM;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.tracom.bms.domain.FM0101.FM0101Service;
+import kr.tracom.cm.support.ControllerSupport;
+
+@Controller
+@Scope("request")
+public class FM0101Controller extends ControllerSupport {
+
+	@Autowired
+	private FM0101Service FM0101Service;
+	
+	@RequestMapping("/fm/FM0101SHI1")
+	public @ResponseBody Map<String, Object> FM0101SHI1() throws Exception {
+		result.setData("dlt_searchitem2", FM0101Service.FM0101SHI1());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/fm/FM0101SHI2")
+	public @ResponseBody Map<String, Object> FM0101SHI2() throws Exception {
+		result.setData("dlt_searchitem3", FM0101Service.FM0101SHI2());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/fm/FM0101G1R0")
+	public @ResponseBody Map<String, Object> FM0101G1R0() throws Exception {
+		result.setData("dlt_BMS_FCLT_INFO", FM0101Service.FM0101G1R0());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/fm/FM0101G2R0")
+	public @ResponseBody Map<String, Object> FM0101G2R0() throws Exception {
+		result.setData("dlt_BMS_FCLT_PARAM_CFG_INFO", FM0101Service.FM0101G2R0());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/fm/FM0101G2S0")
+	public @ResponseBody Map<String, Object> FM0101G2S0() throws Exception {
+		Map map = FM0101Service.FM0101G2S0();
+		result.setData("dma_result", map);
+		return result.getResultSave();
+	}
+	
+	@RequestMapping("/fm/FM0101P0R0")
+	public @ResponseBody Map<String, Object> FM0101P0R0() throws Exception {
+		result.setData("dlt_BMS_FCLT_PARAM_CFG_INFO", FM0101Service.FM0101P0R0());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/fm/FM0101P0SH")
+	public @ResponseBody Map<String, Object> FM0101P0SH() throws Exception {
+		result.setData("dlt_searchitem", FM0101Service.FM0101P0SH());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/fm/FM0101G3R0")
+	public @ResponseBody Map<String, Object> FM0101G3R0() throws Exception {
+		result.setData("dlt_BMS_FCLT_SCH_INFO", FM0101Service.FM0101G3R0());
+		return result.getResult();
+	}
+	
+}
---
+++ BMS-Web/src/main/java/kr/tracom/bms/domain/VD0204/VD0204Service.java
@@ -0,0 +1,96 @@
+package kr.tracom.bms.domain.VD0204;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.tracom.bms.domain.VD0204.VD0204Mapper;
+import kr.tracom.cm.support.ServiceSupport;
+import kr.tracom.cm.support.exception.MessageException;
+import kr.tracom.util.Result;
+
+@Service
+public class VD0204Service extends ServiceSupport {
+	
+	@Autowired
+	private VD0204Mapper vd0204Mapper;
+	
+	public List VD0204G0R0() throws Exception {
+		Map<String, Object> map = getSimpleDataMap("dma_search");
+		return vd0204Mapper.VD0204G0R0(map);
+	}
+
+	public List VD0204SHI0() throws Exception {
+		return vd0204Mapper.VD0204SHI0();
+	}
+	
+	public List VD0204SHI1() throws Exception {
+		return vd0204Mapper.VD0204SHI1();
+	}
+	
+	public List VD0204G1R0() throws Exception {
+		// TODO Auto-generated method stub
+		Map param = getSimpleDataMap("dma_subsearch");
+		return vd0204Mapper.VD0204G1R0(param);
+	}
+	
+	public List VD0204G2R0() throws Exception {
+		// TODO Auto-generated method stub
+		Map param = getSimpleDataMap("dma_subsearch");
+		return vd0204Mapper.VD0204G2R0(param);
+	}
+	
+	public Map VD0204G2S0() throws Exception {
+		int iCnt = 0;
+		int uCnt = 0;
+		int dCnt = 0;		
+		
+		List<Map<String, Object>> param = getSimpleList("dlt_BMS_DVC_PARAM_CFG_INFO");
+		try {
+			for (int i = 0; i < param.size(); i++) {
+				Map data = (Map) param.get(i);
+				String rowStatus = (String) data.get("rowStatus");
+				if (rowStatus.equals("C")) {
+					iCnt += vd0204Mapper.VD0204G2I0(data);
+				} else if (rowStatus.equals("U")) {
+					uCnt += vd0204Mapper.VD0204G2U0(data);
+				} else if (rowStatus.equals("D")) {
+					dCnt += vd0204Mapper.VD0204G2D0(data);
+				} 
+			}			
+		} catch(Exception e) {
+			if (e instanceof DuplicateKeyException)
+			{
+				throw new MessageException(Result.ERR_KEY, "중복된 키값의 데이터가 존재합니다.");
+			}
+			else
+			{
+				throw e;
+			}		
+		}
+
+		
+		Map result = saveResult(iCnt, uCnt, dCnt);
+		
+		return result;		
+		
+		
+	}
+	
+	public List<Map> VD0204P0R0() throws Exception{
+		Map param = getSimpleDataMap("dma_search");
+		return vd0204Mapper.VD0204P0R0(param);
+	}
+	
+	public List VD0204P0SH() throws Exception {
+		return vd0204Mapper.VD0204P0SH();
+	}
+
+	
+
+}
