f2c64ed60821d573b988a75ea3d8a3c423f7847f
hwyang <hwyang@tracom.kr>
2022-02-22 16:13:07 +0900

    [양현우] 안내 컨텐츠 관리 - 안내 컨텐츠 관리(3탭) 페이지 추가

--- BMS-Web/src/main/java/kr/tracom/bms/controller/PI/PI0801Controller.java
+++ BMS-Web/src/main/java/kr/tracom/bms/controller/PI/PI0801Controller.java
@@ -22,6 +22,18 @@ public class PI0801Controller extends ControllerSupport {
 	public @ResponseBody Map<String, Object> PI0801G0R0() throws Exception {
 		result.setData("dlt_BMS_INNER_LED_INFO", pi0801Service.PI0801G0R0());
 		return result.getResult();
+	}
+	
+	@RequestMapping("/pi/PI0801G1R0")
+	public @ResponseBody Map<String, Object> PI0801G1R0() throws Exception {
+		result.setData("dlt_BMS_VOC_INFO", pi0801Service.PI0801G1R0());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/pi/PI0801G2R0")
+	public @ResponseBody Map<String, Object> PI0801G2R0() throws Exception {
+		result.setData("dlt_BMS_VOC_INFO2", pi0801Service.PI0801G2R0());
+		return result.getResult();
 	}	
 
 	@RequestMapping("/pi/PI0801SHI0")
@@ -40,6 +52,43 @@ public class PI0801Controller extends ControllerSupport {
 	@RequestMapping("/pi/PI0801G0K0")
 	public @ResponseBody Map<String, Object> PI0801G0K0() throws Exception {
 		result.setData("dma_SEQ_BMS_INNER_LED_INFO_0", pi0801Service.PI0801G0K0());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/pi/PI0801G1S0")
+	public @ResponseBody Map<String, Object> PI0801G1S0() throws Exception{
+		Map map = pi0801Service.PI0801G1S0();
+		result.setData("dma_result", map);
+		return result.getResultSave();
+	}
+	
+	@RequestMapping("/pi/PI0801G1K0")
+	public @ResponseBody Map<String, Object> PI0801G1K0() throws Exception{
+		result.setData("dma_SEQ_BMS_VOC_INFO_0", pi0801Service.PI0801G1K0());
+		return result.getResult();
+	}	
+
+	@RequestMapping("/pi/PI0801SHI1")
+	public @ResponseBody Map<String, Object> PI0801SHI1() throws Exception {
+		result.setData("dlt_searchitem", pi0801Service.PI0801SHI1());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/pi/PI0801G2S0")
+	public @ResponseBody Map<String, Object> PI0801G2S0() throws Exception{
+		result.setData("result", pi0801Service.PI0801G2S0());
+		return result.getResultSave();
+	}
+	
+	@RequestMapping("/pi/PI0801SHI2")
+	public @ResponseBody Map<String, Object> PI0801SHI2() throws Exception{
+		result.setData("dlt_searchitem", pi0801Service.PI0801SHI2());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/pi/PI0801G2K0")
+	public @ResponseBody Map<String, Object> PI0801G2K0() throws Exception{
+		result.setData("dma_SEQ_BMS_VOC_INFO_3", pi0801Service.PI0801G2K0());
 		return result.getResult();
 	}	
 }
--- BMS-Web/src/main/java/kr/tracom/bms/domain/PI0801/PI0801Mapper.java
+++ BMS-Web/src/main/java/kr/tracom/bms/domain/PI0801/PI0801Mapper.java
@@ -10,6 +10,10 @@ public interface PI0801Mapper {
 	
 	public List PI0801G0R0(Map param);
 	
+	public List PI0801G1R0(Map param);
+	
+	public List PI0801G2R0(Map param);
+	
 	public List PI0801SHI0();
 	
 	public Map PI0801G0K0();
@@ -23,5 +27,25 @@ public interface PI0801Mapper {
 	public int PI0801G0U0(Map param);
 	
 	public int PI0801G0U1(Map param);
+	
+	public List PI0801SHI1();
+	
+	public Map PI0801G1K0();
+	
+	public int PI0801G1I0(Map param);
+	
+	public int PI0801G1D0(Map param);
+	
+	public int PI0801G1U0(Map param);
+	
+	public List PI0801SHI2();
+	
+	public Map PI0801G2K0();
+	
+	public int PI0801G2I0(Map param);
+	
+	public int PI0801G2D0(Map param);
+	
+	public int PI0801G2U0(Map param);	
 	
 }
