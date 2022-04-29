bec10b573780fd464056573375415b59d1d42cbc
bsko <bsko@tracom.kr>
2022-04-29 17:29:26 +0900

    [고병수] bms 수정사항 적용, 배차 시각화 프로그램 생성

---
+++ BMS-Web/src/main/java/kr/tracom/brt/domain/ST0204/ST0204Mapper.java
@@ -0,0 +1,14 @@
+package kr.tracom.brt.domain.ST0204;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ST0204Mapper {
+
+	/*public List ST0202G0R0(Map param);
+	
+	public List ST0202G1R0(Map param);*/
+}
