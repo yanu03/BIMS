84848c28e512e169520a2b83cc52834809fe55ec
bsko <bsko@tracom.kr>
2022-05-20 17:25:23 +0900

    [고병수] 시설물/차내장치 상태 및 제어 수정사항

--- BMS-Web/src/main/java/kr/tracom/bms/domain/VD0204/VD0204Service.java
+++ BMS-Web/src/main/java/kr/tracom/bms/domain/VD0204/VD0204Service.java
@@ -55,42 +55,6 @@ public class VD0204Service extends ServiceSupport {
 		return vd0204Mapper.VD0204G2R1();
 	}
 	
-	public Map VD0204G2S0() throws Exception {
-		int iCnt = 0;
-		int uCnt = 0;
-		int dCnt = 0;		
-		
-		List<Map<String, Object>> param = getSimpleList("dlt_BMS_DVC_PARAM_CFG_INFO");
-		try {
-			for (int i = 0; i < param.size(); i++) {
-				Map data = (Map) param.get(i);
-				String rowStatus = (String) data.get("rowStatus");
-				if (rowStatus.equals("C")) {
-					iCnt += vd0204Mapper.VD0204G2I0(data);
-				} else if (rowStatus.equals("U")) {
-					uCnt += vd0204Mapper.VD0204G2U0(data);
-				} else if (rowStatus.equals("D")) {
-					dCnt += vd0204Mapper.VD0204G2D0(data);
-				} 
-			}			
-		} catch(Exception e) {
-			if (e instanceof DuplicateKeyException)
-			{
-				throw new MessageException(Result.ERR_KEY, "중복된 키값의 데이터가 존재합니다.");
-			}
-			else
-			{
-				throw e;
-			}		
-		}
-
-		
-		Map result = saveResult(iCnt, uCnt, dCnt);
-		
-		return result;		
-		
-		
-	}
 	
 	
 
