7bfc4e7bfe6f8ec765111f5bd6081e21c1ed1b7a
bsj9010 <sjbaek@tracom.kr>
2022-06-16 21:07:41 +0900

    [백승종] 수정 사항 적용

--- BMS-Web/src/main/java/kr/tracom/tims/domain/CurInfoMapper.java
+++ BMS-Web/src/main/java/kr/tracom/tims/domain/CurInfoMapper.java
@@ -13,4 +13,26 @@ public interface CurInfoMapper {
 	//돌발정보 insert
 	int insertIncidentInfo(Map<String, Object> paramMap);
 	
+	//현재운행계획정보 갱신
+	void refreshCurOperAllocPLRoutInfo();
+	void refreshCurOperAllocPLNodeInfo();
+	
+	String getBusId(Map<String, Object> paramMap);
+    Map<String, Object> getRoutMst(Map<String, Object> paramMap);
+    
+    Map<String, Object> selectCurAllocPlInfo(Map<String, Object> paramMap);
+    Map<String, Object> selectCurAllocPlInfoByVhcId(Map<String, Object> paramMap);
+    Map<String, Object> selectCurAllocPlInfoByOperVhcId(Map<String, Object> paramMap);
+    int minAllocNoCurAllocPlInfo(Map<String, Object> params);
+
+	String getCurNearAllocPlInfo(Map<String, Object> paramMap);
+	String getCurNearAllocPlInfo2(Map<String, Object> paramMap);
+    int insertCurAllocPlInfo(Map<String, Object> paramMap);
+    int updateOperVhcIdCurAllocPlInfo(Map<String, Object> paramMap);
+    
+    Map<String, Object> selectCurOperAllocPlRouteInfo(Map<String, Object> paramMap);
+    
+    String getEventCode(Map<String, Object> paramMap);
+    String selectCurNearAllocPl3(Map<String, Object> params);
+
 }
