8f9bb794b805e9c1ecdef48ed4ebce0202b027e8
hwyang <hwyang@tracom.kr>
2022-07-15 11:47:46 +0900

    [양현우] 수정사항 적용

--- BMS-Web/src/main/java/kr/tracom/tims/domain/TimsMapper.java
+++ BMS-Web/src/main/java/kr/tracom/tims/domain/TimsMapper.java
@@ -18,7 +18,8 @@ public interface TimsMapper {
 	Map<String, Object> selectNodeInfo(Map<String, Object> paramMap);	
 
 	//현재노드의 다음노드(교차로 or 정류장) 가져오기
-	Map<String, Object> selectNextSttnCrsInfo(Map<String, Object> paramMap);	
+	Map<String, Object> selectNextSttnCrsInfo(Map<String, Object> paramMap);
+
 	
 	//링크순번으로 노드정보 가져오기
 	Map<String, Object> selectNodeByLinkSn(Map<String, Object> paramMap);
