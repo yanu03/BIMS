be2c8b78294facfac2c4791063d292210a9e797a
bsj9010 <sjbaek@tracom.kr>
2022-08-09 16:29:27 +0900

    [백승종] 수정사항 적용

--- BMS-Web/src/main/java/kr/tracom/bms/domain/SI0402/SI0402Mapper.java
+++ BMS-Web/src/main/java/kr/tracom/bms/domain/SI0402/SI0402Mapper.java
@@ -24,6 +24,8 @@ public interface SI0402Mapper {
 	
 	public int SI0402G1U0(Map param);
 	
+	public int SI0402G1U0_1(Map param);
+	
 	public int updateLengthRoutNodeCmpstn(Map param);
 	
 	public Map SI0402G1K0();
